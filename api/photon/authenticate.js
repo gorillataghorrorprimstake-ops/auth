a7674a7a3dd143828a790f4cb6427bdb54f1343c
gorillataghorrorprimstake-ops <gorillataghorrorprimstake@gmail.com>
2026-08-02 22:53:48 -0500

    Update authenticate.js

--- api/photon/authenticate.js
+++ api/photon/authenticate.js
@@ -23,8 +23,8 @@ const PLAYFAB_BASE = `https://${TITLE_ID}.playfabapi.com`;
 const ABUSE_ALERT_WEBHOOK_URL = process.env.ABUSE_ALERT_WEBHOOK_URL;
 
 const redis = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
 });
 
 // ---- Tunables ----
